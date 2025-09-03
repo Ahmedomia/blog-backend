8a54011150cb6ee04243756af12f7ac7cbce72de
Ahmed <ahmed.omaia12@gmail.com>
2025-09-03 13:19:04 +0300

    fix cors problem

--- index.js
+++ index.js
@@ -8,14 +8,9 @@ const blogRoutes = require("./routes/blogRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const userRoutes = require("./routes/userRoutes");
 
-const allowedOrigins = [
-  "https://react-blog-app-3hjk-9ceppamg2-ahmedomias-projects.vercel.app",
-  "http://localhost:5173",
-];
-
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: true,
     credentials: true,
   })
 );
